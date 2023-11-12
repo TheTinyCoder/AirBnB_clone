aee9eaf5dd1d8393bb14c6f420e0ca695bb9ed43
TheTinyCoder <charawe.yw@gmail.com>
2023-11-12 22:23:14 +0300

    create console

--- console.py
+++ console.py
@@ -6,11 +6,14 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     """Implements functions from cmd"""
     def do_quit(self, line):
+        """Exits the program on quit"""
         return True
     def do_EOF(self, line):
+        """Exits the program after SIGQUIT: CTRL + D"""
         return True
     def prompt = "(hbnb)"
     def emptyline():
+        """Executes nothing if line is empty on enter"""
         pass
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
