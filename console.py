0cb1fd68cafdfa70145f6b1ea2a98f268110832c
TheTinyCoder <charawe.yw@gmail.com>
2023-11-12 22:30:45 +0300

    update prompt in console

--- console.py
+++ console.py
@@ -5,13 +5,13 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """Implements functions from cmd"""
+    prompt = ("(hbnb)")
     def do_quit(self, line):
         """Exits the program on quit"""
         return True
     def do_EOF(self, line):
         """Exits the program after SIGQUIT: CTRL + D"""
         return True
-    def prompt = "(hbnb)"
     def emptyline():
         """Executes nothing if line is empty on enter"""
         pass
